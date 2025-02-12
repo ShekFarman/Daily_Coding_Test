2c76738e09896984325a93a225bf3bfce22fb6c5
ShekFarman <shekfarman18@gmail.com>
2025-02-12 20:01:51 +0530

    Update TransposeMatrix.java

--- TransposeMatrix.java
+++ TransposeMatrix.java
@@ -18,20 +18,20 @@ public class TransposeMatrix {
             }
         }
         
-        // Transpose logic
+       
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 transpose[j][i] = matrix[i][j];
             }
         }
         
-        // Print the transposed matrix
+      
         System.out.println("Transposed Matrix:");
         for (int i = 0; i < cols; i++) {
             for (int j = 0; j < rows; j++) {
                 System.out.print(transpose[i][j] + " ");
             }
-            System.out.println();  // Move to the next line after each row
+            System.out.println(); 
         }
         
         mad.close();
